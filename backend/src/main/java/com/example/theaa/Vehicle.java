fb7244b004bfffd53e7a9ac1bee10228822948bf
Marwan ElKhodary <marwan.elkhodary@gmail.com>
2025-04-27 16:02:27 -0400

    feat: Add basic Vehicle class

---
+++ backend/src/main/java/com/example/theaa/Vehicle.java
@@ -0,0 +1,41 @@
+package com.example.theaa;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String model;
+    private String vrn;
+
+    protected Vehicle() {
+    } // ? Not sure what this does, apparently "exists only for the sake of JPA"
+
+    public Vehicle(String model, String vrn) {
+        this.model = model;
+        this.vrn = vrn;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Vehicle[id=%id, model='%s', lastName='%s']", id, model, vrn);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getVrn() {
+        return vrn;
+    }
+}
