e369e26be4534e83663c83973f28a482e64d71fd
Marwan ElKhodary <marwan.elkhodary@gmail.com>
2025-04-28 08:25:40 -0400

    chore: Update sql load to fit use case

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,14 +1,14 @@
 -- ! Debate if you want to use this or if you want to setup test data like in https://spring.io/guides/gs/accessing-data-jpa
 -- Create a table
-CREATE TABLE Users (
+CREATE TABLE IF NOT EXISTS Vehicle (
     id INT PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100)
+    model VARCHAR(100),
+    vrn VARCHAR(100)
 );
 
 -- Insert a row into the table
-INSERT INTO Users (id, name, email)
-VALUES (1, 'Alice', 'alice@example.com');
+INSERT INTO Vehicle (id, model, vrn)
+VALUES (1, 'Mazda', '123');
 
 -- Select all rows from the table
-SELECT * FROM Users;
+SELECT * FROM Vehicle;
